b03c8566b91d4612b82c924ff27a585481162b2b
HyeYeon <gpdus335@naver.com>
2019-10-30 17:43:31 +0900

    랜덤으로 화살표 생성하는 코드 오류 수정 및 코드 정리

--- Pro_daelim_wego/Assets/Scripts/GameManager.cs
+++ Pro_daelim_wego/Assets/Scripts/GameManager.cs
@@ -13,11 +13,11 @@ public class GameManager : MonoBehaviour
     private Text timeText; // 제한시간 반환
     private float time = 60.0f; // 제한시간
 
-    private int randomLevel = 4; // 출력될 화살표의 클론수
-    private bool gameOver = false; // 게임오버 체크
-    private bool checkFlag = true; // 매 라운드 체크
+    private int randomLevel; // 출력될 화살표의 클론수
+    private bool gameOver; // 게임오버(타임오버) 체크
+    private bool removeFlag; // 매 라운드 게임오버 체크
 
-    public List<string> CheckBlock = new List<string>(); // 랜덤하게 생성될 ArrowClones들의 값 >> 태그로 저장
+    public List<GameObject> CheckBlock = new List<GameObject>(); // 랜덤하게 생성될 ArrowClones들의 값 >> 태그로 저장
     public List<string> PlayerBlock = new List<string>(); // 플레이어의 입력값 저장 
 
     [SerializeField]
@@ -30,7 +30,11 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
+        removeFlag = false;
+
+        randomIndex = Random.Range(0, 3);
+        SetBlock();
     }
 
     // Update is called once per frame
@@ -38,21 +42,14 @@ public class GameManager : MonoBehaviour
     {
         if (!gameOver)
         {
-            if (checkFlag)
-            {
-                randomLevel = Random.Range(3, 6);
-                randomIndex = Random.Range(0, 3);
-                SetBlock();
-            }
-            else
-                PlayerCheck();
+            PlayerCheck();
 
-                time -= Time.deltaTime;
+            time -= Time.deltaTime;
 
             //timeText.text = "" + Mathf.Round(time);
             timeText.text = time.ToString("N0");
         }
-       
+
 
         if (time <= 0)
         {
@@ -64,59 +61,55 @@ public class GameManager : MonoBehaviour
     // 플레이어의 입력값 ArrayList와 랜덤으로 생성된 ArrowClone ArrayList의 각 인덱스 값 비교 후 점수 반환
     public void CheckArrow()
     {
-        //for (int i = 0; i < PlayerBlock.Count; i++)
-        //{
-        //    if (CheckBlock[i] == PlayerBlock[i])
-        //    {
-        //        Debug.Log("score ++ : " + score);
-        //        score += 100;
-        //        scoreText.text = "Score : "+ score;
-                
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("score -- : " + score);
-        //        score -= 100;
-        //        scoreText.text = "Score : " + score;
-                
-        //    }
-        //}
+        if (!removeFlag)
+        {
+            for (int i = 0; i < PlayerBlock.Count; i++)
+            {
+                if (CheckBlock[i].tag == PlayerBlock[i])
+                {
+                    Debug.Log("score ++ : " + score);
+                    score += 100;
+                    scoreText.text = "Score : " + score;
+                    removeFlag = false;
+                }
+                else
+                {
+                    Debug.Log("score -- : " + score);
+                    score -= 100;
+                    scoreText.text = "Score : " + score;
+                    removeFlag = true;
+                    DestroyBlock();
+                    return;
+                }
+            }
 
-        //while(PlayerBlock.Count == CheckBlock.Count)
-        //{
-        //    int i = 0;
-        //    if (CheckBlock[i] == PlayerBlock[i])
-        //    {
-        //        Debug.Log("score ++ : " + score);
-        //        score += 100;
-        //        scoreText.text = "Score : " + score;
-        //        continue;
+        }
 
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("score -- : " + score);
-        //        score -= 100;
-        //        scoreText.text = "Score : " + score;
-        //        continue;
-        //    }
-        //}
-       
     }
 
-    // 랜덤 인덱스를 이용한 클론 화살표 생성 (메뉴바에 생성)
     public void SetBlock()
     {
-        for (int i=0; i < randomLevel+1; i++)
+        randomLevel = Random.Range(3, 6);
+
+        for (int i = 0; i < randomLevel + 1; i++)
         {
-            GameObject cloneArrow = Instantiate(ArrowBlock[randomIndex], new Vector3(100 + (i * 80), 270, 0), Quaternion.identity);
-            CheckBlock.Add(cloneArrow.tag);
-            cloneArrow.transform.SetParent(Arrowclones.transform);
             randomIndex = Random.Range(0, 3);
-            Debug.Log(CheckBlock[i] + " 생성");
+            CheckBlock.Add(Instantiate(ArrowBlock[randomIndex], new Vector3(100 + (i * 80), 270, 0), Quaternion.identity) as GameObject);
+            CheckBlock[i].transform.SetParent(Arrowclones.transform);
+            Debug.Log(" 생성");
         }
-        checkFlag = false;
-       
+    }
+
+    public void DestroyBlock()
+    {
+        for (int i = 0; i < randomLevel + 1; i++)
+        {
+            Destroy(CheckBlock[i]);
+        }
+        CheckBlock.Clear();
+        PlayerBlock.Clear();
+        SetBlock();
+        removeFlag = false;
     }
 
     // 플레이어의 입력값 반환하여 ArrayList에 저장
@@ -142,7 +135,5 @@ public class GameManager : MonoBehaviour
             PlayerBlock.Add("DOWN");
             CheckArrow();
         }
-            
     }
-
 }
