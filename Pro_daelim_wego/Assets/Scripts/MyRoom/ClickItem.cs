c62cef4dc7caa588a63df8d58350a828aabf61ce
HyeYeonCha <gpdus335@naver.com>
2019-11-07 17:33:44 +0900

    아까 날아간거 복구 및 스크롤 추가

--- Pro_daelim_wego/Assets/Scripts/MyRoom/ClickItem.cs
+++ Pro_daelim_wego/Assets/Scripts/MyRoom/ClickItem.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ClickItem : MonoBehaviour
+using UnityEngine.EventSystems;
+public class ClickItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,31 @@ public class ClickItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ItemClick();
+    }
+
+    public void ItemClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click : " + gameObject.name);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Click : " + gameObject.name);
     }
 }
