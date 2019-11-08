81baeccb215196309cbfe70c2146b5ed3dd7d5bd
HyeYeonCha <gpdus335@naver.com>
2019-11-08 17:47:24 +0900

    툴팁 마우스 좌표로 수정

--- Pro_daelim_wego/Assets/Scripts/MyRoom/Tooltip.cs
+++ Pro_daelim_wego/Assets/Scripts/MyRoom/Tooltip.cs
@@ -14,29 +14,37 @@ public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     private Item item;
 
+    Vector2 MousPosition;
+    Camera Camera;
+
     // Start is called before the first frame update
     void Start()
     {
         toolTip.SetActive(false);
         item = gameObject.GetComponent<Item>();
+        Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MousPosition = Input.mousePosition;
+        MousPosition = Camera.ScreenToWorldPoint(MousPosition);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log(gameObject.name);
         toolTip.SetActive(true);
-        //toolTip.transform.position = new Vector3(Input.) // >> 툴팁의 포지션을 마우스 좌표의 옆으로 바꿔주기
+        toolTip.transform.position = new Vector2(MousPosition.x-1f, MousPosition.y-1f); // >> 툴팁의 포지션을 마우스 좌표의 옆으로 바꿔주기
         toolTipTextString = "" + item.ItemInfo.itemDesc;
         toolTipTextUI.text = toolTipTextString;
+        Debug.Log("On");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         toolTip.SetActive(false);
+        Debug.Log("Off");
     }
 }
